89c7086d609793420561797f79420475d95b6e8e
iampaz3 <>
2021-06-04 04:00:48 +0000

    Defined the answer to question

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,8 +6,8 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question 
-let correctAnswer
-let candidateAnswer
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = "Sally Ride";
 let questions;
 let correctAnswers
 let candidateAnswers
@@ -15,13 +15,13 @@ let candidateAnswers
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question("What is your name?");
+candidateName = input.question("What is your name? ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-question = input.question("Who was the first American woman in sapce?");
+question = input.question("Who was the first American woman in sapce? ");
 
 }
 
@@ -29,7 +29,7 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 if (candidateAnswer === "") {
-  console.log("That is the correct answer!");
+  console.log("That would be correct!");
 }else {
  console.log("That would be incorrect!"); 
 }
